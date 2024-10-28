5628806b47fd24fe7539b6c43160eea9202e5a2a
CJRiney <chrisriney4@gmail.com>
2024-10-28 21:59:13 +0000

    hid the key

--- tests/nearby_houses_test.py
+++ tests/nearby_houses_test.py
@@ -4,7 +4,8 @@ import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from create_dataset.house_dataset_creator import HouseDatasetCreator
 
-creator = HouseDatasetCreator('AIzaSyCOUyvZo0K1lobh_wMK4oqzwCaV8xzxKhk')
+api_key = os.getenv("GOOGLE_API_KEY")
+creator = HouseDatasetCreator(api_key)
 
 iphone_res = '3024x4032'
 address = '6682 Trigo Rd, Goleta, CA 93117', '3080 Deseret Dr, El Sobrante, CA 94803'
